5f349dd9bab8f8487af284507430922f380a2827
David Virgilio <drvirgilio@gmail.com>
2022-03-12 14:16:27 -0600

    Initial commit with example code from https://dev.to/fabioarnold/setup-zig-for-gamedev-2bmf

---
+++ build.zig
@@ -0,0 +1,15 @@
+const std = @import("std");
+
+pub fn build(b: *std.build.Builder) void {
+    const target = b.standardTargetOptions(.{ .default_target = .{ .abi = .gnu } });
+    const mode = b.standardReleaseOptions();
+
+    const exe = b.addExecutable("snake", "src/main.zig");
+    exe.setTarget(target);
+    exe.setBuildMode(mode);
+
+    exe.linkSystemLibrary("sdl2");
+    exe.linkLibC();
+    exe.install();
+}
+
